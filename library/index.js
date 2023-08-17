2bbee278c98ab959b11e50f8726ebdbe621ea13e
Maksim Litvinov <lmk478shade@gmail.com>
2023-08-17 21:45:48 +0300

    feat: mobile slides arrows and dots

--- library/index.js
+++ library/index.js
@@ -6,6 +6,7 @@ console.log(
   'Самопроверка, часть 2:\nВёрстка соответствует макету. Ширина экрана 768px +26\nНи на одном из разрешений до 640px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +12\nНа ширине экрана 768рх реализовано адаптивное меню +12'
 );
 
+//----------------- Side menu -----------------
 let height = window.innerHeight;
 let width = window.innerWidth;
 
@@ -57,3 +58,55 @@ function preventScroll(e) {
   e.stopPropagation();
   return false;
 }
+
+//----------------- Slider -----------------
+let currentPosition = 0;
+const NEXT = 'next';
+const PREV = 'prev';
+const DOT = 'dot';
+const slides = document.querySelectorAll(".about__list_slider-item");
+const dots = document.querySelectorAll(".about__list_slider-dot");
+const left_arrow = document.getElementById('about__arrow_left');
+const right_arrow = document.getElementById('about__arrow_right');
+
+
+const renderSlide = (action, index) => {
+  if((index === slides.length-1 && action === NEXT) || (index === 0 && action === PREV)) {
+    alert("OI!")
+  } else {
+    if(action===NEXT){
+      slides[index].classList.add("about__list_slider-item-hidden")
+      slides[index+1].classList.remove("about__list_slider-item-hidden")
+      dots[index].classList.remove("about__list_slider-dot_active")
+      dots[index+1].classList.add("about__list_slider-dot_active")
+      currentPosition++
+      
+    }
+    if(action===PREV){
+      slides[index].classList.add("about__list_slider-item-hidden")
+      slides[index-1].classList.remove("about__list_slider-item-hidden")
+      dots[index].classList.remove("about__list_slider-dot_active")
+      dots[index-1].classList.add("about__list_slider-dot_active")
+      currentPosition--
+    }
+    if(action===DOT){
+      slides[currentPosition].classList.add("about__list_slider-item-hidden")
+      slides[index].classList.remove("about__list_slider-item-hidden")
+      dots[currentPosition].classList.remove("about__list_slider-dot_active")
+      dots[index].classList.add("about__list_slider-dot_active")
+      currentPosition=index;
+    }
+  }
+  
+  console.log(currentPosition)
+}
+
+right_arrow.addEventListener('click', ()=> renderSlide(NEXT, currentPosition));
+left_arrow.addEventListener('click',()=>  renderSlide(PREV, currentPosition));
+
+dots.forEach((dot,index)=>{
+  dot.addEventListener('click', ()=>renderSlide(DOT, index))
+})
+
+
+console.log(slides)
