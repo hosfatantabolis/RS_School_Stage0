e98c8bffe58fe7f136704fa9608f57b6dbb119c1
Maksim Litvinov <lmk478shade@gmail.com>
2023-09-22 00:58:39 +0300

    feat: play/pause/next/prev buttons

--- js30_1.2-audio-player/scripts/index.js
+++ js30_1.2-audio-player/scripts/index.js
@@ -2,4 +2,49 @@ import  {audioContents, playBtn, prevBtn, nextBtn} from './constants.js';
 
 console.log(playBtn);
 
-playBtn.addEventListener('click', playMusic);
+const audio = new Audio();
+let position = 0;
+let isPlaying = false;
+
+audio.src = audioContents[position].src;
+audio.volume = 0.2;
+
+console.log(audio.src);
+function playAudio() {
+    if(isPlaying){
+        audio.pause();
+    } else {
+        audio.play();
+    }
+    isPlaying = !isPlaying;
+    //console.log(audio.currentTime);
+}
+
+function nextSong() {
+    isPlaying = !isPlaying;
+    if (position < audioContents.length - 1) position += 1;
+    else position = 0;
+    audio.src = audioContents[position].src;
+    playAudio();
+}
+
+function prevSong() {
+    isPlaying = !isPlaying;
+    if (position > 0) position -= 1;
+    else position = audioContents.length - 1;
+    audio.src = audioContents[position].src;
+    playAudio();
+}
+
+playBtn.addEventListener('click', () => {
+    playBtn.classList.toggle('audioPlayer__pause');
+    playAudio();
+});
+
+nextBtn.addEventListener('click', () => {
+    nextSong();
+});
+
+prevBtn.addEventListener('click', () => {
+    prevSong();
+});
