bb83466f34d1531e1743c303d505ae5889bab832
Maksim Litvinov <lmk478shade@gmail.com>
2023-09-21 22:15:55 +0300

    fix: paths fix

--- js30_1.2-audio-player/scripts/constants.js
+++ js30_1.2-audio-player/scripts/constants.js
@@ -1,25 +1,25 @@
 export const audioContents = [
     {
-        src: '../assets/audio/A-Ha - Forest Fire.mp3',
-        cover: '../assets/img/a-ha.jpg',
+        src: './assets/audio/A-Ha - Forest Fire.mp3',
+        cover: './assets/img/a-ha.jpg',
         artist: 'A-Ha',
         song: 'Forest Fire'
     },
     {
-        src: '../assets/audio/Elbow - Magnificent (She Says).mp3',
-        cover: '../assets/img/elbow.jpg',
+        src: './assets/audio/Elbow - Magnificent (She Says).mp3',
+        cover: './assets/img/elbow.jpg',
         artist: 'Elbow',
         song: 'Magnificent (She Says)'
     },
     {
-        src: '../assets/audio/Mr. Oizo feat. Sebastien Tellier - Stunt.mp3',
-        cover: '../assets/img/oizo.jpg',
+        src: './assets/audio/Mr. Oizo feat. Sebastien Tellier - Stunt.mp3',
+        cover: './assets/img/oizo.jpg',
         artist: 'Mr. Oizo feat. Sebastien Tellier',
         song: 'Stunt'
     },
     {
-        src: '../assets/audio/Tuff Steppas feat. Gregory G Ras - Mr. Leader (Hasta la Vista).mp3',
-        cover: '../assets/img/tuff.jpg',
+        src: './assets/audio/Tuff Steppas feat. Gregory G Ras - Mr. Leader (Hasta la Vista).mp3',
+        cover: './assets/img/tuff.jpg',
         artist: 'Tuff Steppas feat. Gregory G Ras',
         song: 'Mr. Leader (Hasta la Vista)'
     },
