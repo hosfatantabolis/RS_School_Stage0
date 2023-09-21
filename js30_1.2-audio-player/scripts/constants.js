ca5356594e5cd4660467cbe67c08854b1729a937
Maksim Litvinov <lmk478shade@gmail.com>
2023-09-21 22:10:22 +0300

    feat: scripts folder restructuring

---
+++ js30_1.2-audio-player/scripts/constants.js
@@ -0,0 +1,30 @@
+export const audioContents = [
+    {
+        src: '../assets/audio/A-Ha - Forest Fire.mp3',
+        cover: '../assets/img/a-ha.jpg',
+        artist: 'A-Ha',
+        song: 'Forest Fire'
+    },
+    {
+        src: '../assets/audio/Elbow - Magnificent (She Says).mp3',
+        cover: '../assets/img/elbow.jpg',
+        artist: 'Elbow',
+        song: 'Magnificent (She Says)'
+    },
+    {
+        src: '../assets/audio/Mr. Oizo feat. Sebastien Tellier - Stunt.mp3',
+        cover: '../assets/img/oizo.jpg',
+        artist: 'Mr. Oizo feat. Sebastien Tellier',
+        song: 'Stunt'
+    },
+    {
+        src: '../assets/audio/Tuff Steppas feat. Gregory G Ras - Mr. Leader (Hasta la Vista).mp3',
+        cover: '../assets/img/tuff.jpg',
+        artist: 'Tuff Steppas feat. Gregory G Ras',
+        song: 'Mr. Leader (Hasta la Vista)'
+    },
+];
+
+export const playBtn = document.getElementById('playBtn');
+export const prevBtn = document.getElementById('prevBtn');
+export const nextBtn = document.getElementById('nextBtn');
---
+++ js30_1.2-audio-player/scripts/index.js
@@ -0,0 +1,5 @@
+import  {audioContents, playBtn, prevBtn, nextBtn} from './constants.js';
+
+console.log(playBtn);
+
+playBtn.addEventListener('click', playMusic);
