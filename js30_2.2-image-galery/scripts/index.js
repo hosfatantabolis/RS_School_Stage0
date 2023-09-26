5d32cf8ffa8995b1f956064ec58c22ca6b7efba8
Maksim Litvinov <lmk478shade@gmail.com>
2023-09-26 16:41:23 +0300

    feat: search function

--- js30_2.2-image-galery/scripts/index.js
+++ js30_2.2-image-galery/scripts/index.js
@@ -36,26 +36,63 @@ const api = new Api;
 const cardElements = document.querySelector('.cardList');
 const popup = document.getElementById('popup');
 const popupImg = popup.querySelector('img');
+const headerForm = document.getElementById('headerForm');
+const searchField = document.getElementById('headerForm');
+
+const createImgLi = (card) => {
+  let li = document.createElement('li');
+  li.classList.add('cardList__item');
+  let img = document.createElement('img');
+  img.src = `${card.urls.regular}`;
+  img.alt = `${card.alt_description}`;
+  img.classList.add('cardList__image');
+  li.append(img);
+  li.addEventListener('click', (e) => {
+    popup.classList.add('popup__open');
+    popupImg.src = e.target.src;
+    popup.addEventListener('click', (e) => {
+     if(e.target !== popupImg) popup.classList.remove('popup__open')
+    })
+  })
+  cardElements.append(li);
+}
 
 api.getImages()
 .then(cards => {
-    console.log(cards);
     cards.map(card => {
-        let li = document.createElement('li');
-        li.classList.add('cardList__item');
-        let img = document.createElement('img');
-        img.src = `${card.urls.regular}`;
-        img.alt = `${card.alt_description}`;
-        img.classList.add('cardList__image');
-        li.append(img);
-        li.addEventListener('click', (e) => {
-          popup.classList.add('popup__open');
-          popupImg.src = e.target.src;
-          popup.addEventListener('click', (e) => {
-           if(e.target !== popupImg) popup.classList.remove('popup__open')
-          })
-        })
-        cardElements.append(li);
+        createImgLi(card);
     })
 })
 
+headerForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const inputs = Object.fromEntries(formData);
+  if (inputs.searchF!==''){
+    api.searchImages(inputs.searchF).then(res => {
+      cardElements.replaceChildren([]);
+      if(res.length!==0){
+        cardElements.classList.remove('cardList_empty');
+        res.map(card => {
+          createImgLi(card);
+      })
+      } else {
+        cardElements.classList.add('cardList_empty');
+        let p = document.createElement('p');
+        p.classList.add('cardList__text');
+        p.textContent='No images found';
+        cardElements.append(p);
+      }
+    })
+  } else {
+      api.getImages()
+      .then(cards => {
+          console.log(cards);
+          cardElements.replaceChildren([]);
+          cardElements.classList.remove('cardList_empty');
+          cards.map(card => {
+              createImgLi(card);
+          })
+      })
+    }
+})
